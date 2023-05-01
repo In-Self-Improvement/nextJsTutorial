60dd49b9da5a1e8e24c07dbc6bd3827fda7cd002
Bong <borobong230@gmail.com>
2023-05-01 21:19:13 +0900

    fix: Add project of nextjs

---
+++ utils/projectType.ts
@@ -0,0 +1,115 @@
+{
+  "object": "page",
+  "id": "8dc65f51-13ba-46f9-a2b5-c40c750aedf9",
+  "created_time": "2023-04-19T15:35:00.000Z",
+  "last_edited_time": "2023-04-29T20:13:00.000Z",
+  "created_by": {
+      "object": "user",
+      "id": "b3b11f2b-0ebe-48d1-9cbf-23031019c775"
+  },
+  "last_edited_by": {
+      "object": "user",
+      "id": "b3b11f2b-0ebe-48d1-9cbf-23031019c775"
+  },
+  "cover": {
+      "type": "external",
+      "external": {
+          "url": "https://www.notion.so/images/page-cover/rijksmuseum_rembrandt_1642.jpg"
+      }
+  },
+  "icon": null,
+  "parent": {
+      "type": "database_id",
+      "database_id": "281c9390-49d3-4576-b050-610e6eb74e5a"
+  },
+  "archived": false,
+  "properties": {
+      "WorkPeriod": {
+          "id": "Be%7D%3C",
+          "type": "date",
+          "date": {
+              "start": "2023-04-03",
+              "end": "2023-04-06",
+              "time_zone": null
+          }
+      },
+      "Tags": {
+          "id": "H%3BP%3E",
+          "type": "multi_select",
+          "multi_select": [
+              {
+                  "id": "5b64f3e6-4655-47d3-bcde-ad4fd873b5d8",
+                  "name": "Next.js",
+                  "color": "purple"
+              },
+              {
+                  "id": "8d2a62bd-25c6-43ef-99e3-1c35eba997b2",
+                  "name": "TailwindCss",
+                  "color": "yellow"
+              },
+              {
+                  "id": "3202c648-6a97-4043-a99a-8b430cd9c632",
+                  "name": "빡코딩",
+                  "color": "yellow"
+              }
+          ]
+      },
+      "Youtube": {
+          "id": "Qq%5D%5B",
+          "type": "url",
+          "url": "https://borobong230.tistory.com/"
+      },
+      "Description": {
+          "id": "sMD%7C",
+          "type": "rich_text",
+          "rich_text": [
+              {
+                  "type": "text",
+                  "text": {
+                      "content": "desc",
+                      "link": null
+                  },
+                  "annotations": {
+                      "bold": false,
+                      "italic": false,
+                      "strikethrough": false,
+                      "underline": false,
+                      "code": false,
+                      "color": "default"
+                  },
+                  "plain_text": "desc",
+                  "href": null
+              }
+          ]
+      },
+      "Github": {
+          "id": "yeLv",
+          "type": "url",
+          "url": "https://github.com/borobong2"
+      },
+      "Name": {
+          "id": "title",
+          "type": "title",
+          "title": [
+              {
+                  "type": "text",
+                  "text": {
+                      "content": "프로젝트 01",
+                      "link": null
+                  },
+                  "annotations": {
+                      "bold": false,
+                      "italic": false,
+                      "strikethrough": false,
+                      "underline": false,
+                      "code": false,
+                      "color": "default"
+                  },
+                  "plain_text": "프로젝트 01",
+                  "href": null
+              }
+          ]
+      }
+  },
+  "url": "https://www.notion.so/01-8dc65f5113ba46f9a2b5c40c750aedf9"
+}
